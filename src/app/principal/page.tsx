d3179f71ace98db9c52c239bb926d938e7f39c49
Felipe Zündler <felipezundlercontato@gmail.com>
2024-11-28 20:27:08 -0300

    Update page.tsx

--- src/app/principal/page.tsx
+++ src/app/principal/page.tsx
@@ -103,8 +103,8 @@ export default function Principal() {
         </div>
       </div>
 
-      <h2 className="text-2xl font-bold mt-4 bg-transparent">Gráfico: Nº de Produtos por Marca</h2>
-      <Chart chartType="ColumnChart" width="95%" height="380px" data={data} options={options} />
+      <h2 className="text-2xl font-bold mt-4">Gráfico: Nº de Produtos por Marca</h2>
+      <Chart bg-transparent chartType="ColumnChart" width="95%" height="380px" data={data} options={options} />
     </div>
   );
 }
